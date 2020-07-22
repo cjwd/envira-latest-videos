35120bbcf42049e17aca9b098e8dea71353772cd
cjwd <chinarakinda@gmail.com>
2020-07-22 17:51:25 -0400

    Remove console log statement

--- lightbox.js
+++ lightbox.js
@@ -1,23 +1,22 @@
-document.addEventListener("click", lightboxClick);
+document.addEventListener('click', lightboxClick);
 
 function lightboxClick(e) {
-  console.log(e.target);
   var el = e.target,
-    elID = el.getAttribute("id"),
-    lightboxIframe = document.getElementById("lightbox-iframe"),
-    lightbox = document.getElementById("lightbox-overlay"),
+    elID = el.getAttribute('id'),
+    lightboxIframe = document.getElementById('lightbox-iframe'),
+    lightbox = document.getElementById('lightbox-overlay'),
     body = document.body;
 
-  if (el.hasAttribute("data-lightbox")) {
+  if (el.hasAttribute('data-lightbox')) {
     e.preventDefault();
 
-    lightboxIframe.setAttribute("src", el.getAttribute("data-lightbox"));
-    lightbox.classList.add("is-visible");
-    body.classList.add("no-scroll");
+    lightboxIframe.setAttribute('src', el.getAttribute('data-lightbox'));
+    lightbox.classList.add('is-visible');
+    body.classList.add('no-scroll');
   }
 
-  if (elID == "lightbox-close" || elID == "lightbox-overlay") {
-    lightbox.classList.remove("is-visible");
-    body.classList.remove("no-scroll");
+  if (elID == 'lightbox-close' || elID == 'lightbox-overlay') {
+    lightbox.classList.remove('is-visible');
+    body.classList.remove('no-scroll');
   }
 }
